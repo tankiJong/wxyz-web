d051bb10ad15fccc0fc1a9a748ac124858df2bd8
Tanki Zhang <tianyiz@nvidia.com>
2026-01-12 00:17:41 -0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://wxyz.foundation',
+  trailingSlash: 'ignore',
+  build: {
+    assets: '_astro'
+  }
 });
